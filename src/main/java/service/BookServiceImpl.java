9d2f8d1390f66000511ad810f2e892953a86305b
oksana <slovachevska.ok@gmail.com>
2023-09-25 16:04:51 +0300

    added book entity

---
+++ src/main/java/service/BookServiceImpl.java
@@ -0,0 +1,29 @@
+package service;
+
+import model.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.BookRepository;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+}
